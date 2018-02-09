70c2faa5ef91c96128055c09ea379cd4c986d167
Yu <yliu@talend.com>
2018-02-09 16:00:29 +0100

    update

--- main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.ps1
+++ main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.ps1
@@ -1,1 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
 java ${talend.job.jvmargs} -cp ${talend.job.bat.classpath} ${talend.job.class} ${talend.job.bat.addition} %* 
