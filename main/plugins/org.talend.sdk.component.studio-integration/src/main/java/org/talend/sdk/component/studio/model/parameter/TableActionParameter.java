8788c912575503341315452bbc021c4d92816d18
Ivan Honchar <ivan.honchar@globallogic.com>
2018-08-09 17:26:24 +0300

    fix(TCOMP-936): add TableElementParameter junits

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/TableActionParameter.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/TableActionParameter.java
@@ -21,14 +21,16 @@ class TableActionParameter extends ActionParameter {
     public Collection<Pair<String, String>> parameters() {
         final List<Map<String, String>> value = (List<Map<String, String>>) elementParameter.getValue();
         final List<Pair<String, String>> parameters = new ArrayList<>();
-        for (int i=0; i<value.size(); i++) {
-            final Map<String, String> row = value.get(i);
-            for(Map.Entry<String, String> entry : row.entrySet()) {
-                String key = entry.getKey();
-                key = key.replace("[]", "[" + i + "]").replace(elementParameter.getName(), getParameter());
-                final String paramValue = removeQuotes(entry.getValue());
-                final Pair parameter = new Pair(key, paramValue);
-                parameters.add(parameter);
+        if (value != null) {
+            for (int i=0; i<value.size(); i++) {
+                final Map<String, String> row = value.get(i);
+                for(Map.Entry<String, String> entry : row.entrySet()) {
+                    final String key = entry.getKey().replace("[]", "[" + i + "]")
+                            .replace(elementParameter.getName(), getParameter());
+                    final String paramValue = removeQuotes(entry.getValue());
+                    final Pair parameter = new Pair(key, paramValue);
+                    parameters.add(parameter);
+                }
             }
         }
         return parameters;
